e64a30cccdd0e0e477b1b950a973ac48802dd906
redkritz09 <112176330+Maximillian360@users.noreply.github.com>
2023-02-25 21:44:56 +0800

    Update index.php

--- Num2/index.php
+++ Num2/index.php
@@ -1,9 +1,6 @@
 <?php
     require_once "class2.php";
 
-    $opt = isset($_POST["selectedOption"] ) ? $_POST["selectedOption"]: '';
-    $prodMsg = "";
-    $prodDetails ="";
 
     if(isset($_POST['submit'])){
         if(isset($_POST['name']) && isset($_POST['price']) && isset($_POST['creator']) && isset($_POST['identifier'])){
@@ -11,14 +8,14 @@
             $price = $_POST['price'];
             $creator = $_POST['creator'];
             $identifier = $_POST['identifier'];
+            $opt = $_POST['selectedOption'];
 
-            $choice = $opt == "1" ? new Book($name, $price, $creator, $identifier) : new Movies($creator, $identifier);
+            $product = $opt == "Book" ? new Book($name, $price, $creator, $identifier) : new Movie($name, $price, $creator, $identifier);
         }
         else{
-            echo "error!";
+            echo "Error!";
         }
     }
-
 
     ?>
 
@@ -32,30 +29,39 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="d-flex align-items-center justify-content-center vh-100" style="flex-direction: column;">
-            <div class="mb-3">
-                <select class="form-select" name="selectedOption">
-                    <option value="1">Book</option>
-                    <option value="2">Movie</option>
-                </select>
-            </div>
-            <br>
-            <div>
-                Product Name: <input type="text" name="name">
+        <form method="POST">
+            <div class="d-flex align-items-center justify-content-center vh-100" style="flex-direction: column;">
+                <div class="mb-3" style="text-align: center">
+                    <select class="form-select" name="selectedOption">
+                        <option value="Book">Book</option>
+                        <option value="Movie">Movie</option>
+                    </select>
+                </div>
                 <br>
-                Product Price: <input type="text" name="price">
-            </div>
-            <br>
-            <div>
-                Director/Author: <input type="text" name="creator">
+                <div>
+                    Product Name: <input type="text" name="name">
+                    <br>
+                    Product Price: <input type="number" name="price">
+                </div>
                 <br>
-                Rating/Genre: <input type="text" name="identifier">
+                <div>
+                    Director/Author: <input type="text" name="creator">
+                    <br>
+                    Rating/Genre: <input type="text" name="identifier">
+                </div>
+                <div>
+                    <input type="submit" value="Submit" name="submit" class="btn btn-primary">
+                </div>
+
+                <div class="mb-3" style="text-align: center">
+                    <?php echo "<br>Resulting Product is: <br>". $product->display()?>
+                </div>
             </div>
-            <div>
-                <input type="submit" value="Submit" name="submit">
-            </div>
-        </div>
-        <?php var_dump() ?>
+
+
+
+        </form>
+
 
 
 
